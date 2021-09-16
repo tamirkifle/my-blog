f5655c3bd3fbfaf9d52e79ddcf3d57ebd05677fd
Tamir K. Yirga <tkifle@gmail.com>
2021-09-16 14:48:06 +0300

    initialized scully with md files

--- scully.my-blog.config.ts
+++ scully.my-blog.config.ts
@@ -4,5 +4,11 @@ export const config: ScullyConfig = {
   projectName: "my-blog",
   outDir: './dist/static',
   routes: {
+    '/posts/:id': {
+      type: 'contentFolder',
+      id: {
+        folder: "./mdfiles"
+      }
+    },
   }
 };
